427d5a7da76eaf4ea51950051b743caffea3585a
Sebastian Osorio <sebaoso@gmail.com>
2023-02-26 01:17:18 +0100

    Add test

--- src/test/java/se/osorio/football/FootballApplicationTests.java
+++ src/test/java/se/osorio/football/FootballApplicationTests.java
@@ -1,23 +1,47 @@
 package se.osorio.football;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import se.osorio.football.controller.FootballRestController;
+import se.osorio.football.model.Player;
 import se.osorio.football.model.Team;
 import se.osorio.football.repository.TeamRepository;
+import se.osorio.football.service.PlayerService;
 
+import java.util.Arrays;
 import java.util.List;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
-@SpringBootTest(classes = { FootballApplication.class })
-@ExtendWith(SpringExtension.class)
+//@SpringBootTest(classes = { FootballApplication.class })
+//@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class FootballApplicationTests {
+
+  @MockBean
+  private PlayerService playerServiceMock;
 
   @Autowired
   private TeamRepository teamRepository;
+
+  @Autowired
+  private MockMvc mvc;
+
+  @BeforeEach
+  public void initialiseRestAssuredMockMvcStandalone(){
+    MockitoAnnotations.openMocks(this);
+  }
 
   @Test
   public void testGetTeamsByName() {
@@ -25,4 +49,25 @@ public class FootballApplicationTests {
     List<Team> teams = controller.getTeamsByName("Argentina");
     assertEquals(1, teams.size());
   }
+
+  @Test
+  public void givenStartpage_shouldExistTitle() throws Exception {
+
+    mvc.perform(MockMvcRequestBuilders.get("/football"))
+            .andExpect(MockMvcResultMatchers.content().string(containsString("FIFA World Cup 2022")));
+  }
+
+  @Test
+  public void givenCountryArgentina_shouldExistMessi() throws Exception {
+
+    List<Player> playerList = Arrays.asList(Player.builder().name("Lionel Messi").club("PSG").position("RW").country("Argentina").build(),
+            Player.builder().name("Angel Di Maria").club("Juventus").position("LW").country("Argentina").build());
+
+    when(playerServiceMock.getPlayersFromCountry("Argentina")).thenReturn(playerList);
+
+    mvc.perform(MockMvcRequestBuilders.get("/players?country=Argentina"))
+            .andExpect(MockMvcResultMatchers.content().string(containsString("Lionel Messi")));
+  }
+
+
 }
